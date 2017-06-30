2737f6ab2f88ab1805f78d0c9adb44ef3dabdca0
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2017-06-30 03:40:56 +0200

    Add sudo again

--- lib/bash/rancher_ci_bootstrap.sh
+++ lib/bash/rancher_ci_bootstrap.sh
@@ -109,7 +109,7 @@ docker_lvm_thinpool_config() {
     sudo tee /etc/sysconfig/docker-storage <<-EOF
 DOCKER_STORAGE_OPTIONS=--storage-driver=devicemapper --storage-opt=dm.thinpooldev=/dev/mapper/docker-thinpool --storage-opt dm.use_deferred_removal=true
 EOF
-mkdir -p /etc/docker
+sudo mkdir -p /etc/docker
     sudo cat <<EOF >> /etc/docker/daemon.json
 {
 "storage-driver": "devicemapper",
