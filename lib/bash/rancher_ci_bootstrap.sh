7cf2fc30262b9a8cf4dae00911168d14e70c6186
Hussein Galal <galal-hussein@users.noreply.github.com>
2017-07-12 04:27:37 +0300

    Remove Puppet Code from RHEL docker installation (#5)
    * Replace puppet code in RHEL
    
    * Add sudo
    
    * Add daemon.json
    
    * Add sudo
    
    * add cat
    
    * Add mkdir for old docker version
    
    * Add sudo again
    
    * replace cat
    
    * remove creating yum repo function
    
    * Add stop docker after installation

--- lib/bash/rancher_ci_bootstrap.sh
+++ lib/bash/rancher_ci_bootstrap.sh
@@ -81,78 +81,34 @@ ec2_tag_get_docker_version() {
 
 
 ###############################################################################
-# add RHEL Docker yum config
-###############################################################################
-docker_repo_yum_create() {
-    sudo tee /etc/yum.repos.d/docker.repo <<-EOF 
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-    sudo yum clean all -y
-    sudo yum makecache
-}
-
-
-###############################################################################
-# add RHEL Docker yum config
-###############################################################################
-docker_version_match_rhel() {
-    local docker_version
-    docker_version="$(ec2_tag_get_docker_version)" || exit $?
-    
-    docker_version_match="$(sudo yum --showduplicates list docker-engine  | grep ${docker_version} | sort -rn | head -n1 | awk -F' ' '{print $2}')" || exit $?
-    if [ -z "${docker_version_match}" ]; then
-	echo "Failed while detecting a distro package match for specified Docker version '${docker_version}'!"
-	exit 1
-    fi
-
-    echo "${docker_version_match}"
-    
-}
-
-
-###############################################################################
 # Docker volume LVM adjustments done the right way. :\
 ###############################################################################
 docker_lvm_thinpool_config() {
     local docker_version
     docker_version="$(ec2_tag_get_docker_version)" || exit $?
 
-    docker_repo_yum_create
-    local docker_verison_match
-    docker_version_match="$(docker_version_match_rhel)" || exit $?
+    wget -O - "https://releases.rancher.com/install-docker/${docker_version}.sh" | sudo bash -
 
-    sudo puppet module install puppetlabs/stdlib
-    sudo puppet module install garethr/docker
-
-    tee /tmp/docker_config.pp <<-PUPPET
-class { ::docker:
-  ensure => '${docker_version_match}',
-  repo_opt => '',
-#  tcp_bind => ['tcp://0.0.0.0:2376'],
-#  tls_enable => true,
-#  tls_cacert => '/etc/docker/ca.pem',
-#  tls_cert => '/etc/docker/server.pem',
-#  tls_key => '/etc/docker/server-key.pem',
-  storage_driver => 'devicemapper',
-  storage_vg => 'docker',
-  dm_thinpooldev => '/dev/mapper/docker-thinpool',
+    sudo systemctl stop docker
+    
+    sudo tee /etc/sysconfig/docker-storage <<-EOF
+DOCKER_STORAGE_OPTIONS=--storage-driver=devicemapper --storage-opt=dm.thinpooldev=/dev/mapper/docker-thinpool --storage-opt dm.use_deferred_removal=true
+EOF
+    sudo mkdir -p /etc/docker
+    sudo tee /etc/docker/daemon.json <<-EOF
+{
+"storage-driver": "devicemapper",
+"storage-opts": [
+   "dm.thinpooldev=/dev/mapper/docker-thinpool",
+   "dm.use_deferred_removal=true",
+   "dm.use_deferred_deletion=true"
+ ]
 }
-PUPPET
+EOF
+    sudo rm -rf /var/lib/docker
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker
 
-    set +e
-    sudo puppet apply --verbose --detailed-exitcodes /tmp/docker_config.pp
-    set -e
-    sudo puppet apply --verbose --detailed-exitcodes /tmp/docker_config.pp
-
-    sudo puppet resource service docker ensure=stopped
-    sudo rm -rf /var/lib/docker/network
-    sudo ip link del docker0
-    sudo puppet resource service docker ensure=running
 }
 
 
@@ -192,13 +148,13 @@ system_prep() {
             sudo wget -O /etc/yum.repos.d/epel.repo https://mirror.openshift.com/mirror/epel/epel7.repo
 	    sudo yum install -y deltarpm
 	    sudo yum upgrade -y
-	    
+
 	    # if this worked we could have inspec. :\
 	    #	    sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
 	    #	    sudo yum install -y ruby-devel
 	    #	    sudo yum groupinstall -y "Development Tools"
 	    #	    sudo yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel
-	    
+
 	    sudo yum install --skip-broken -y jq python-pip htop puppet python-docutils mosh
 	    sudo puppet resource service puppet ensure=stopped enable=false
 	    sudo pip install awscli
@@ -214,7 +170,7 @@ system_prep() {
 	    sudo apt-get install -y jq awscli htop mosh cloud-guest-utils puppet
 	    sudo puppet resource service puppet ensure=stopped enable=false
 	    ;;
-	
+
 	default)
 	    ;;
     esac
@@ -231,10 +187,10 @@ redhat_config() {
     #	sudo sed -i -e 's/sslverify=1/sslverify=0/g' "/etc/yum.repos.d/${file}"
     #done
     #    sudo yum remove rh-amazon-rhui-client -y
-    
+
     sudo yum clean all -y
     sudo yum makecache
-    
+
     sudo yum install -y lvm2
     sudo pvcreate -ff -y /dev/xvdb
     sudo vgcreate docker /dev/xvdb
@@ -273,10 +229,10 @@ main() {
 	echo 'Performing special RHEL osfamily storage config...'
 	redhat_config
 	docker_lvm_thinpool_config
-	
+
     elif [ 'debian' == "${osfamily}" ]; then
 	docker_install_tag_version
-	
+
     else
 	echo "OS family \'${osfamily}\' will default to vendor supplied and pre-installed Docker engine."
     fi
