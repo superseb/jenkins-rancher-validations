a659dc0e32af0a78b92ed154955eb8cf2f971dc2
Sangeetha Hariharan <sangeetha@rancher.com>
2018-11-02 14:15:52 -0700

    Add support for rosv1.4.2 (#33)

--- lib/python/utils/__init__.py
+++ lib/python/utils/__init__.py
@@ -251,6 +251,10 @@ def os_to_settings(os):
         ami = 'ami-08ca2e89d91d17cfe'
         ssh_username = 'rancher'
 
+    elif 'rancheros-v1.4.2' in os:
+        ami = 'ami-03c3efd1e21bb4c6d'
+        ssh_username = 'rancher'
+
     elif 'rancheros-v1.1.3' in os:
         ami = 'ami-e316a39b'
         ssh_username = 'rancher'
